a51f2c25f739d8b650e7fd3d8671013af9bbd7e4
varla <varlamov577123@gmail.com>
2023-11-08 22:24:09 +0300

    И еще одна попытка

--- make_logs.py
+++ make_logs.py
@@ -4,7 +4,8 @@ import sys
 
 class Logger:
     def __init__(self) -> None:
-        self.logger = logging.getLogger()
+        self.logger_info = logging.getLogger()
+        self.logger_warn = logging.getLogger()
 
         self.handler_stdout = logging.StreamHandler(stream=sys.stdout)
         self.handler_to_file = logging.FileHandler("./in_work.log")
@@ -13,11 +14,16 @@ class Logger:
         self.handler_stdout.setFormatter(self.formatter)
         self.handler_to_file.setFormatter(self.formatter)
 
-        self.logger.addHandler(self.handler_stdout)
-        self.logger.addHandler(self.handler_to_file)
+        self.logger_warn.addHandler(self.handler_stdout)
+        self.logger_warn.addHandler(self.handler_to_file)
+        self.logger_info.addHandler(self.handler_stdout)
+        self.logger_info.addHandler(self.handler_to_file)
+
+        self.logger_info.level = logging.INFO
+        self.logger_warn.level = logging.WARNING
 
     def make_warn_log(self, message: str) -> None:
-        self.logger.warning(message, "WARN")
+        self.logger_warn.warning(message)
 
     def make_info_log(self, message: str) -> None:
-        self.logger.info(message, "INFO")
+        self.logger_info.info(message)
