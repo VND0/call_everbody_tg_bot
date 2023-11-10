29fb818ec9a0d0c90c38bb49a436bf0418ea81c2
varla <varlamov577123@gmail.com>
2023-11-10 20:07:16 +0300

    Нет, не заработало

--- make_logs.py
+++ make_logs.py
@@ -9,17 +9,22 @@ class Logger:
         self.logger_info.setLevel(logging.INFO)
         self.logger_warn.setLevel(logging.WARNING)
 
-        self.stdout_stream = logging.StreamHandler(sys.stdout)
-        self.file_stream = logging.FileHandler("in_work.log", encoding="utf-8")
+        self.stdout_stream_w = logging.StreamHandler(sys.stdout)
+        self.file_stream_w = logging.FileHandler("in_work.log", encoding="utf-8")
+        self.stdout_stream_i = logging.StreamHandler(sys.stdout)
+        self.file_stream_i = logging.FileHandler("in_work.log", encoding="utf-8")
+
         self.format = logging.Formatter(datefmt="%d_%m_%Y %I:%M:%S", fmt="%(asctime)s [%(levelname)s]: %(message)s")
 
-        self.stdout_stream.setFormatter(self.format)
-        self.file_stream.setFormatter(self.format)
+        self.stdout_stream_w.setFormatter(self.format)
+        self.file_stream_w.setFormatter(self.format)
+        self.stdout_stream_i.setFormatter(self.format)
+        self.file_stream_i.setFormatter(self.format)
 
-        self.logger_warn.addHandler(self.stdout_stream)
-        self.logger_warn.addHandler(self.file_stream)
-        self.logger_info.addHandler(self.stdout_stream)
-        self.logger_info.addHandler(self.file_stream)
+        self.logger_warn.addHandler(self.stdout_stream_w)
+        self.logger_warn.addHandler(self.file_stream_w)
+        self.logger_info.addHandler(self.stdout_stream_i)
+        self.logger_info.addHandler(self.file_stream_i)
 
     def make_warn_log(self, message: str) -> None:
         self.logger_warn.warning(message)
